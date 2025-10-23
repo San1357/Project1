6042ba5b5ffc358c8ef857ff1713c8f3e4d863c5
Aayush Gupta <you@example.com>
2025-10-23 15:18:40 +0530

    Move Predict component to src/pages and update imports

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import Predict from "./Predict";
+import Predict from "./pages/Predict";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
